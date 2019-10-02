f1501100f9a2e7c7a2a9909d1c02e0355fe62eb6
honeywine <49772278+honeywine@users.noreply.github.com>
2019-10-02 00:29:04 -0400

    Update environment.prod.ts

--- SupportForce/src/environments/environment.prod.ts
+++ SupportForce/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
   BACKEND_URL: 'localhost:8080'
-  /* place actual backend url thats not local host...
-    also possibly no port number???
-  */
 };
